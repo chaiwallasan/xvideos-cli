3ef31c8066a2b4985da81b9a16c357750025b722
rikardhassel <rikard.hassel@softcode.se>
2015-01-04 23:17:37 +0200

    Change output

--- bin/xvideos.js
+++ bin/xvideos.js
@@ -27,27 +27,11 @@ if (Object.hasOwnProperty.call(argv, "help") || Object.hasOwnProperty.call(argv,
     durf: argv.duration || "allduration",
   };
 
-  tty.text("Searching for: ").bold().text(options.k).normal().text(" (" + xv.constructSearchUrl(options) + ")\n");
-
   xv.search(options, function(err, res) {
     if (err) {
       return console.warn(err);
     }
-
-    tty.text("Got ").bold().text(res.videos.length.toString(10)).normal().text(" results. Getting details...\n\n");
-
-    res.videos.forEach(function(video) {
-      xv.details(video.url, function(err, details) {
-        if (err) {
-          return;
-        }
-
-        tty.colour(5).text(details.title).reset().text("\n");
-        tty.bold().text(video.url + "\n").normal();
-        tty.text("[ " + details.tags.join(", ") + " ]\n");
-        tty.bold().text("FLV: ").normal().text(details.flv + "\n");
-        tty.text("\n");
-      });
-    });
+    
+    console.log( res );
   });
 }
